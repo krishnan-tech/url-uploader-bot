744bb3f8ed669f6b19319efbbff4f457b002d3ac
Rugved Koshiya <rugvedkoshiya2002@gmail.com>
2021-08-04 18:56:28 +0530

    bug fix and optimization

--- main.py
+++ main.py
@@ -25,7 +25,14 @@ def start(update: Update, context: CallbackContext) -> None:
 
 def upload_siasky(file_path):
     skylink = client.upload_file(file_path)
-    return skylink[6:]
+    return SETTING.SIASKY_CLIENT + skylink[6:]
+
+def create_upload_folder():
+    if not os.path.exists("uploads"):
+        os.makedirs("uploads")
+
+def remove_uploaded_file(file_name):
+    os.remove(f'uploads/{file_name}') # delete downloaded file from server
 
 def main_function(update: Update, context: CallbackContext) -> None:
     # ['text', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'message_auto_delete_timer_changed', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message', 'poll', 'dice', 'passport_data', 'proximity_alert_triggered', 'voice_chat_scheduled', 'voice_chat_started', 'voice_chat_ended', 'voice_chat_participants_invited', 'audio', 'game', 'animation', 'document', 'photo', 'sticker', 'video', 'voice', 'video_note', 'contact', 'location', 'venue', 'invoice', 'successful_payment']
@@ -33,64 +40,58 @@ def main_function(update: Update, context: CallbackContext) -> None:
     # if input is photo
     if len(update.message.photo) > 0:
 
-        if not os.path.exists("photos"):
-            os.makedirs("photos")
-
+        create_upload_folder() # create upload folder if not exists
         file_unique_id = (update.message.photo[-1]['file_unique_id']) # get file ID
         
         update.message.reply_text("Image Downloading... 😎")
-        update.message.photo[-1].get_file().download(f"photos/{file_unique_id}") # download image
+        update.message.photo[-1].get_file().download(f"uploads/{file_unique_id}") # download image
 
         update.message.reply_text("Image Uploading... 📤")
-        update.message.reply_text(f"{SETTING.PHOTO_MSG}\n{SETTING.SIASKY_CLIENT}{upload_siasky(f'photos/{file_unique_id}')}") # upload image
+        update.message.reply_text(f"{SETTING.PHOTO_MSG}\n{upload_siasky(f'uploads/{file_unique_id}')}") # upload image
 
-        os.remove(f'photos/{file_unique_id}') # delete downloaded image from server
+        remove_uploaded_file(file_unique_id)
     
     # if input is video
     elif update.message.video != None:
 
-        if not os.path.exists("videos"):
-            os.makedirs("videos")
-
+        create_upload_folder() # create upload folder if not exists
         file_unique_id = (update.message.video['file_unique_id']) # get file ID
 
         update.message.reply_text("Video Downloading... 😎")
-        update.message.video.get_file().download(f"videos/{file_unique_id}") # download video
+        update.message.video.get_file().download(f"uploads/{file_unique_id}") # download video
 
         update.message.reply_text("Video Uploading... 📤")
-        update.message.reply_text(f"{SETTING.VIDEO_MSG}\n{SETTING.SIASKY_CLIENT}{upload_siasky(f'videos/{file_unique_id}')}") # upload video
+        update.message.reply_text(f"{SETTING.VIDEO_MSG}\n{upload_siasky(f'uploads/{file_unique_id}')}") # upload video
 
-        os.remove(f'videos/{file_unique_id}') # delete downloaded video from server
+        remove_uploaded_file(file_unique_id)
 
     # if input is document
     elif update.message.document != None:
 
-        if not os.path.exists("documents"):
-            os.makedirs("documents")
+        create_upload_folder() # create upload folder if not exists
         file_unique_id = (update.message.document['file_unique_id']) # get file ID
 
         update.message.reply_text("File Downloading... 😎")
-        update.message.document.get_file().download(f"documents/{file_unique_id}") # download file
+        update.message.document.get_file().download(f"uploads/{file_unique_id}") # download file
 
         update.message.reply_text("File Uploading... 📤")
-        update.message.reply_text(f"{SETTING.DOCUMENT_MSG}\n{SETTING.SIASKY_CLIENT}{upload_siasky(f'documents/{file_unique_id}')}") # upload file
+        update.message.reply_text(f"{SETTING.DOCUMENT_MSG}\n{upload_siasky(f'uploads/{file_unique_id}')}") # upload file
 
-        os.remove(f'videos/{file_unique_id}') # delete downloaded document from server
+        remove_uploaded_file(file_unique_id)
 
     # if input is audio
     elif update.message.audio != None:
 
-        if not os.path.exists("audios"):
-            os.makedirs("audios")
-        file_unique_id = (update.message.audio['file_unique_id']) # get file ID
+        create_upload_folder() # create upload folder if not exists
+        file_unique_id = (update.message.audio['file_unique_id']) # get file id
 
         update.message.reply_text("Audio Downloading... 😎")
-        update.message.audio.get_file().download(f"audios/{file_unique_id}") # download file
+        update.message.audio.get_file().download(f"uploads/{file_unique_id}") # download file
 
         update.message.reply_text("Audio Uploading... 📤")
-        update.message.reply_text(f"{SETTING.AUDIO_MSG}\n{SETTING.SIASKY_CLIENT}{upload_siasky(f'audios/{file_unique_id}')}") # upload audio
+        update.message.reply_text(f"{SETTING.AUDIO_MSG}\n{upload_siasky(f'uploads/{file_unique_id}')}") # upload audio
 
-        os.remove(f'audios/{file_unique_id}') # delete downloaded audio from server
+        remove_uploaded_file(file_unique_id)
 
     # TODO : Refactor code
 
@@ -109,12 +110,5 @@ def main() -> None:
     updater.idle()
 
 
-def test():
-    skylink = client.upload_file("image.jpg")
-    print(skylink)
-    skylink = client.upload_file("video.mp4")
-    print(skylink)
-
 if __name__ == '__main__':
     main()
-    # test()
