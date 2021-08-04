7b6008279d90038268984a445bf6274398b8b7c1
krishnan-tech <krishnannavadia@gmail.com>
2021-08-04 20:14:23 +0530

    remove import

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import logging
 import os
 from dotenv import load_dotenv
 from config import Config as SETTING
-from telegram import Update, ForceReply
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
 import siaskynet as skynet
 
 load_dotenv()
